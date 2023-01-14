2c82d7a5b1ccda3154a1af7300b156e93fd58c00
dromakin <d.romakin@npo-echelon.ru>
2023-01-14 22:15:58 +0300

    Fix Main.java, add init schema to GraphicsConverter

--- src/ru/netology/graphics/Main.java
+++ src/ru/netology/graphics/Main.java
@@ -1,21 +1,12 @@
 package ru.netology.graphics;
 
-import ru.netology.graphics.image.ColorSchema;
 import ru.netology.graphics.image.GraphicsConverter;
-import ru.netology.graphics.image.TextColorSchema;
 import ru.netology.graphics.image.TextGraphicsConverter;
 import ru.netology.graphics.server.GServer;
 
 public class Main {
     public static void main(String[] args) throws Exception {
         TextGraphicsConverter converter = new GraphicsConverter(); // Создайте тут объект вашего класса конвертера
-        TextColorSchema schema = new ColorSchema();
-
-//        converter.setMaxWidth(200);
-//        converter.setMaxHeight(1000);
-//        converter.setMaxRatio(2);
-
-        converter.setTextColorSchema(schema);
 
         GServer server = new GServer(converter); // Создаём объект сервера
         server.start(); // Запускаем
